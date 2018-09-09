a6dd6fd58335800199e32e29d459af3c10dac918
Gaëtan Varlet <gaetanvarlet@gmail.com>
2018-09-09 19:52:38 +0200

    ajout de l'installation d'une fausse API avec JSON Placeholder

---
+++ tutoriel-video/projet-boite-a-post/fillDB.js
@@ -0,0 +1,13 @@
+var casual = require('casual')
+
+module.exports = () => {
+    const data = { posts: [] }
+    for (let i = 0; i < 25; i++) {
+      data.posts.push({
+        id: i, title: casual.title,
+        content: casual.sentences(n=10),
+        author: casual.name
+      })
+    }
+    return data
+  }
